e7a5ec750ad6e045f74d323e67358ba7862523b5
김병희 <k8z7@hanmail.net>
2016-09-22 17:15:18 +0900

    20160922 레이아웃 onscreen.xml(ImageView) 사용, 오른쪽 위에서 50dp 배치

--- app/src/main/java/net/kbh/ontop/AlwaysOnTopService.java
+++ app/src/main/java/net/kbh/ontop/AlwaysOnTopService.java
@@ -1,16 +1,17 @@
 package net.kbh.ontop;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
-import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 public class AlwaysOnTopService extends Service {
-    private TextView tv;											//항상 보이게 할 뷰
+    private View mView; //항상 보이게 할 뷰
+
     @Override
     public IBinder onBind(Intent arg0) { return null; }
 
@@ -18,10 +19,8 @@ public class AlwaysOnTopService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        tv = new TextView(this);		//뷰 생성
-        tv.setText("이 뷰는 항상 위에 있다.");
-        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-        tv.setTextColor(Color.BLUE);
+        LayoutInflater mInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mView = mInflater.inflate(R.layout.onscreen, null);
 
         //최상위 윈도우에 넣기 위한 설정
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
@@ -30,16 +29,15 @@ public class AlwaysOnTopService extends Service {
                 PixelFormat.TRANSLUCENT);													//투명
 
         WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);	//윈도우 매니저 불러옴.
-        wm.addView(tv, params);		//최상위 윈도우에 뷰 넣기. *중요 : 여기에 permission을 미리 설정해 두어야 한다. 매니페스트에
+        wm.addView(mView, params);		//최상위 윈도우에 뷰 넣기. *중요 : 여기에 permission을 미리 설정해 두어야 한다. 매니페스트에
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(tv != null)	//서비스 종료시 뷰 제거. *중요 : 뷰를 꼭 제거 해야함.
-        {
-            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(tv);
-            tv = null;
+        if(mView != null) { //서비스 종료시 뷰 제거. *중요 : 뷰를 꼭 제거 해야함.
+            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(mView);
+            mView = null;
         }
     }
 
